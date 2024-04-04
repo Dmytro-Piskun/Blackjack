0c80fae5a2c49bc60b71041234bc39596804c30c
Dima <dmitriy.piskun.dp@gmail.com>
2024-04-04 20:11:10 +0300

    first commit

--- blackjack-react/src/App.jsx
+++ blackjack-react/src/App.jsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
   path: "/",
   errorElement: <ErrorPage/>,
   // loader: () => {return redirect("/auth")},// check if logged in 
+  //fgdgd
   },
   {
     path: "auth",
